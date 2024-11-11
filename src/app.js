6d3c5b42e8ebf9a510c193ad265b5d44adb7b528
uttamchess <utthamchess@gmail.com>
2024-11-11 21:42:59 +0530

    explorimg get,post,delete

--- src/app.js
+++ src/app.js
@@ -2,21 +2,25 @@ const express = require("express");
 
 const app = express();
 
-app.use("/hello/2",(req, res) => {
-  res.send("Hello from the hello page 2");
+app.get("/user", (req, res) => {
+  res.send({
+    firstName: "Uttham",
+    lastName: "Naresh",
+  });
 });
 
-app.use("/hello",(req, res) => {
-  res.send("Hello from the hello page");
+app.delete("/user", (req, res) => {
+  res.send("Deleted Sucessfully");
 });
 
-app.use("/test",(req, res) => {
+app.post("/user", (req, res) => {
+  console.log("Saving data to database");
+  res.send("Data Sucessfully saved in the database");
+});
+
+app.use("/test", (req, res) => {
   res.send("Hello from the server");
 });
- 
-// app.use("/",(req, res) => {
-//   res.send("Hello from the home");
-// });
 
 app.listen(7777, () => {
   console.log("Server sucessfully started!!");
