3e2422465cecc96d13a3abaed5dcccc4e432054e
Uttham Chess <utthamchess@gmail.com>
2025-07-11 13:38:42 +0530

    created /signup, /user, /feed APIs sucessfully

--- src/app.js
+++ src/app.js
@@ -3,19 +3,40 @@ const { connectDB } = require("./config/database");
 const app = express();
 const User = require("./models/user");
 
+app.use(express.json());
+
 app.post("/signup", async (req, res) => {
   //Creating a new instance of the user model
-  const user = new User({
-    firstName: "Akshay",
-    lastName: "Saini",
-    emailId: "akshaysaini@gmail.com",
-    password: "akshay!123",
-  });
+  const user = new User(req.body);
   try {
-    await user.save;
+    await user.save();
     res.send("User Added sucesussfully");
   } catch (err) {
     res.status(400).send("Error saving the user: " + err.message);
+  }
+});
+
+//Get user by email
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+  try {
+    const user = await User.findOne({ emailId: userEmail });
+    if (!user) {
+      res.send("User not found");
+    } else {
+      res.send(user);
+    }
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("Something went wrong");
   }
 });
 
