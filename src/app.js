6ae42d5e9942c17fcdb07b2097bfa2dd56eb48a2
Uttham Chess <utthamchess@gmail.com>
2025-07-13 11:04:05 +0530

    validations

--- src/app.js
+++ src/app.js
@@ -3,6 +3,10 @@ const { connectDB } = require("./config/database");
 const app = express();
 const User = require("./models/user");
 
+// app.use((req, res) => {
+//   res.send("Hi!!!!");
+// });
+
 app.use(express.json());
 
 app.post("/signup", async (req, res) => {
@@ -13,6 +17,30 @@ app.post("/signup", async (req, res) => {
     res.send("User Added sucesussfully");
   } catch (err) {
     res.status(400).send("Error saving the user: " + err.message);
+  }
+});
+
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    const user = await User.findByIdAndDelete(userId);
+    res.send("User deleted sucefully");
+  } catch (err) {
+    res.status(400).send("User Deleted Sucessfully");
+  }
+});
+
+app.patch("/user", async (req, res) => {
+  const user = req.body.userId;
+  const data = req.body;
+  try {
+    await User.findByIdAndUpdate({ _id: user }, data, {
+      returnDocument: "after",
+      runValidators: true,
+    });
+    res.send("User updates sucessfully");
+  } catch (err) {
+    res.status(400).send("Something went wrong");
   }
 });
 
@@ -43,8 +71,8 @@ app.get("/feed", async (req, res) => {
 connectDB()
   .then(() => {
     console.log("Database sucessfully established");
-    app.listen(3000, () => {
-      console.log("Server Successfully started on port number: 3000");
+    app.listen(7777, () => {
+      console.log("Server Successfully started on port number: 7777");
     });
   })
   .catch(() => {
--- src/models/user.js
+++ src/models/user.js
@@ -3,12 +3,15 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   firstName: {
     type: String,
+    required: true,
   },
   lastName: {
     type: String,
   },
   emailId: {
     type: String,
+    required: true,
+    unique: true,
   },
   password: {
     type: String,
@@ -18,7 +21,22 @@ const userSchema = new mongoose.Schema({
   },
   gender: {
     type: String,
+    validate(value) {
+      if (!["male", "female", "others"].includes(value)) {
+        throw new Error("Gender data is not valid");
+      }
+    },
+  },
+  photoUrl: {
+    type: String,
+  },
+  about: {
+    type: String,
+    default: "This is the default description of the user",
+  },
+  skills: {
+    type: [String],
   },
 });
 
-module.exports = mongoose.model("User",userSchema);
+module.exports = mongoose.model("User", userSchema);
